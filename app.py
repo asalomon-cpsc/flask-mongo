6c66b01f3845a8d3c13d1d6beb9fc3d1657ead77
asalomon-cpsc <asalomon@cpsc.gov>
2019-12-17 07:42:41 -0500

    cleaned up comment

--- app.py
+++ app.py
@@ -16,35 +16,13 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    # try:
-    # write to DB
-    # get db
 
     result = []
     for report in mongo.db.report.find():
-        print(report)
-
         result.append({'first_name': report['FirstName'],
                        'last_name': report['LastName'],
                        'email': report['Email'],
                        'state': report['State'],
                        'injury': report['Injury']})
- # except Exception:
-        # call this method if any of the database operation above fail
 
-        # Prepare the response
     return jsonify(result)
-
- # finally:
-   # print('Process completed')
-
-
-# serve(app, host="0.0.0.0", port='5000')
-
-# if __name__ == '__main__':
-#   HOST = environ.get('SERVER_HOST', '0.0.0.0')
-#   try:
-#       PORT = int(environ.get('SERVER_PORT', '5555'))
-#  except ValueError:
-#      PORT = 5555
-#   app.run(HOST, PORT)
